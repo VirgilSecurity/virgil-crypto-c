538cce22c456aafa7af5220083ab740b408493b5
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-05-08 18:22:54 +0300

    Add module 'memory' (codegen)

---
+++ include/virgil/vsf_memory.h
@@ -0,0 +1,131 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Provides configurable memory management model.
+// --------------------------------------------------------------------------
+
+#ifndef VSF_MEMORY_H_INCLUDED
+#define VSF_MEMORY_H_INCLUDED
+
+#include "vsf_library.h"
+//  @end
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//  @generated
+// --------------------------------------------------------------------------
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Compile-time configuration of the default alloc function.
+//
+#ifndef VSF_ALLOC_DEFAULT
+#   define VSF_ALLOC_DEFAULT(size) calloc (1, (size))
+#endif
+
+//
+//  Compile-time configuration of the default dealloc function.
+//
+#ifndef VSF_DEALLOC_DEFAULT
+#   define VSF_DEALLOC_DEFAULT(mem) free ((mem))
+#endif
+
+//
+//  Allocate required amount of memory by usging current allocation function.
+//  Returns NULL if memory allocation fails.
+//
+VSF_PUBLIC const void*
+vsf_alloc (size_t size);
+
+//
+//  Deallocate given memory by usging current de-allocation function.
+//
+VSF_PUBLIC void
+vsf_dealloc (void* mem);
+
+//
+//  Change current used memory functions in the runtime.
+//
+VSF_PUBLIC void
+vsf_set_allocators (vsf_alloc_fn alloc_cb, vsf_dealloc_fn dealloc_cb);
+
+//
+//  Zeroize memory.
+//  Note, this function can be reduced by compiler during optimization step.
+//  For sensitive data erasing use vsf_erase ().
+//
+VSF_PUBLIC void
+vsf_zeroize (void* mem, size_t size);
+
+//
+//  Zeroize memory in a secure manner.
+//  Compiler can not reduce this function during optimization step.
+//
+VSF_PUBLIC void
+vsf_erase (void* mem, size_t size);
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// --------------------------------------------------------------------------
+//  @end
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+//  @footer
+#endif // VSF_MEMORY_H_INCLUDED
+//  @end
