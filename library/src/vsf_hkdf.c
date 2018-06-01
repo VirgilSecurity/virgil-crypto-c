6e7e01b8ea0e537fac6d3ddaea919920aa8cb68d
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-06-01 15:31:41 +0300

    Declare HKDF private methods in the implementation model

--- library/src/vsf_hkdf.c
+++ library/src/vsf_hkdf.c
@@ -63,6 +63,20 @@
 //  Generated section start.
 // --------------------------------------------------------------------------
 
+//
+//  Extracts fixed-length pseudorandom key from keying material.
+//
+static void
+vsf_hkdf_extract (vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, const byte* salt,
+        size_t salt_len, byte* pr_key, size_t pr_key_len);
+
+//
+//  Expands the pseudorandom key to the desired length.
+//
+static void
+vsf_hkdf_expand (vsf_hkdf_impl_t* hkdf_impl, const byte* pr_key, size_t pr_key_len,
+        const byte* info, size_t info_len, byte* key, size_t key_len);
+
 
 // --------------------------------------------------------------------------
 //  Generated section end.
@@ -70,20 +84,26 @@
 //  @end
 
 
+//
+//  Extracts fixed-length pseudorandom key from keying material.
+//
+static void
+vsf_hkdf_extract (vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, const byte* salt,
+        size_t salt_len, byte* pr_key, size_t pr_key_len) {
 
-
-VSF_PRIVATE void
-extract (vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, const byte* salt, size_t salt_len, byte* pr_key, size_t pr_key_len)
-{
     vsf_hmac_stream_reset (hkdf_impl->hmac);
     vsf_hmac_stream_start (hkdf_impl->hmac,salt,salt_len);
     vsf_hmac_stream_update (hkdf_impl->hmac, data, data_len);
     vsf_hmac_stream_finish (hkdf_impl->hmac, pr_key, pr_key_len);
 }
 
-VSF_PRIVATE void
-expand (vsf_hkdf_impl_t* hkdf_impl, const byte*  pr_key, size_t pr_key_len, const byte*  info, size_t info_len, byte* key, size_t key_len)
-{
+//
+//  Expands the pseudorandom key to the desired length.
+//
+static void
+vsf_hkdf_expand (vsf_hkdf_impl_t* hkdf_impl, const byte* pr_key, size_t pr_key_len,
+        const byte* info, size_t info_len, byte* key, size_t key_len) {
+
     size_t offset = 0;
     unsigned char counter = 0x00;
     size_t current_hmac_len = vsf_hmac_info_digest_size (vsf_hmac_info_api (hkdf_impl->hmac));
@@ -109,14 +129,13 @@ expand (vsf_hkdf_impl_t* hkdf_impl, const byte*  pr_key, size_t pr_key_len, cons
         {
             memcpy (key + offset, current_hmac, need);
             offset += need;
-        }           
+        }
     } while (offset < key_len);
 }
 
 //
 //  Calculate hash over given data.
 //
-
 VSF_PUBLIC void
 vsf_hkdf_derive (vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, const byte* salt,
         size_t salt_len, const byte* info, size_t info_len, byte* key, size_t key_len) {
@@ -131,9 +150,9 @@ vsf_hkdf_derive (vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, 
     size_t pr_key_len = vsf_hmac_info_digest_size (vsf_hmac_info_api (hkdf_impl->hmac));
     unsigned char* pr_key = vsf_alloc (pr_key_len);
 
-    extract (hkdf_impl, data, data_len, salt, salt_len, pr_key, pr_key_len);
+    vsf_hkdf_extract (hkdf_impl, data, data_len, salt, salt_len, pr_key, pr_key_len);
     if (key_len < 255*pr_key_len)
-        expand (hkdf_impl, pr_key, pr_key_len, info, info_len, key, key_len);
+        vsf_hkdf_expand (hkdf_impl, pr_key, pr_key_len, info, info_len, key, key_len);
 
 
     vsf_dealloc (pr_key);
