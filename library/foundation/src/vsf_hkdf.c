e9c61fa65e91866ef1e1f4a2f696ca7759f9d6d3
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-06-05 17:15:55 +0300

    Add constant 'hash counter max' to the 'hkdf' implementation

--- library/foundation/src/vsf_hkdf.c
+++ library/foundation/src/vsf_hkdf.c
@@ -65,6 +65,13 @@
 // --------------------------------------------------------------------------
 
 //
+//  Private integral constants.
+//
+enum {
+    vsf_hkdf_HASH_COUNTER_MAX = 255
+};
+
+//
 //  Extracts fixed-length pseudorandom key from keying material.
 //
 static void
@@ -142,17 +149,14 @@ vsf_hkdf_derive(vsf_hkdf_impl_t* hkdf_impl, const byte* data, size_t data_len, c
     VSF_ASSERT_PTR(key);
 
     size_t pr_key_len = vsf_hmac_info_digest_size(vsf_hmac_info_api(hkdf_impl->hmac));
+    VSF_ASSERT_OPT(key_len <= vsf_hkdf_HASH_COUNTER_MAX * pr_key_len);
 
     unsigned char* pr_key = vsf_alloc(pr_key_len);
-
     VSF_ASSERT_PTR(pr_key);
 
     vsf_hkdf_extract(hkdf_impl, data, data_len, salt, salt_len, pr_key, pr_key_len);
-    if (key_len < 255 * pr_key_len) {
-        vsf_hkdf_expand(hkdf_impl, pr_key, pr_key_len, info, info_len, key, key_len);
-    } else {
-        VSF_ASSERT_OPT("Key size is large!!!");
-    }
+
+    vsf_hkdf_expand(hkdf_impl, pr_key, pr_key_len, info, info_len, key, key_len);
 
     vsf_dealloc(pr_key);
 }
