9706f115b466e6dfcccd1961869eb02dd27ef96e
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-06-05 14:41:45 +0300

    Add project 'pythia' skeleton

---
+++ library/pythia/include/virgil/private/vsc_pythia_impl_private.h
@@ -0,0 +1,122 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  This module contains Private API common for all 'implementation' objects.
+// --------------------------------------------------------------------------
+
+#ifndef VSC_PYTHIA_IMPL_PRIVATE_H_INCLUDED
+#define VSC_PYTHIA_IMPL_PRIVATE_H_INCLUDED
+
+#include "vsc_pythia_library.h"
+#include "vsc_pythia_error.h"
+#include "vsc_pythia_impl.h"
+//  @end
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Contains common properties for any 'API' implementation object.
+//
+struct vsc_pythia_impl_info_t {
+    //
+    //  Implementation unique identifier, MUST be first in the structure.
+    //
+    vsc_pythia_impl_tag_t impl_tag;
+    //
+    //  NULL terminated array of the implemented interfaces.
+    //  MUST be second in the structure.
+    //
+    const vsc_pythia_api_t** api_array;
+    //
+    //  Erase inner state in a secure manner.
+    //
+    vsc_pythia_impl_cleanup_fn self_cleanup_cb;
+    //
+    //  Self destruction, according to destruction policy.
+    //
+    vsc_pythia_impl_delete_fn self_delete_cb;
+};
+typedef struct vsc_pythia_impl_info_t vsc_pythia_impl_info_t;
+
+//
+//  Contains header of any 'API' implementation structure.
+//  It is used for runtime type casting and checking.
+//
+struct vsc_pythia_impl_t {
+    //
+    //  Compile-time known information.
+    //
+    const vsc_pythia_impl_info_t* info;
+};
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+//  @footer
+#endif // VSC_PYTHIA_IMPL_PRIVATE_H_INCLUDED
+//  @end
---
+++ library/pythia/src/vsc_pythia_assert.c
@@ -0,0 +1,136 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Implements custom assert mechanism, which:
+//      - allows to choose assertion handler from predefined set,
+//        or provide custom assertion handler;
+//      - allows to choose which assertion leave in production build.
+// --------------------------------------------------------------------------
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+#include "vsc_pythia_assert.h"
+
+#include <stdio.h>
+//  @end
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Return pointer to the last component in the path.
+//
+static const char*
+vsc_pythia_assert_path_basename(const char* path);
+
+//
+//  Active handler for assertion failback.
+//
+static vsc_pythia_assert_handler_fn active_handler = vsc_pythia_assert_abort;
+
+//
+//  Change active assertion handler.
+//
+VSC_PYTHIA_PUBLIC void
+vsc_pythia_assert_change_handler(vsc_pythia_assert_handler_fn handler_cb) {
+
+    VSC_PYTHIA_ASSERT (handler_cb);
+    active_handler = handler_cb;
+}
+
+//
+//  Assertion handler, that print given information and abort program.
+//  This is default handler.
+//
+VSC_PYTHIA_PUBLIC void
+vsc_pythia_assert_abort(const char* message, const char* file, int line) {
+
+    printf ("Assertion failed: %s, file %s, line %d\n",
+            message, vsc_pythia_assert_path_basename (file), line);
+
+    printf ("Abort");
+
+    abort ();
+}
+
+//
+//  Trigger active assertion handler.
+//
+VSC_PYTHIA_PUBLIC void
+vsc_pythia_assert_trigger(const char* message, const char* file, int line) {
+
+    active_handler (message, file, line);
+}
+
+//
+//  Return pointer to the last component in the path.
+//
+static const char*
+vsc_pythia_assert_path_basename(const char* path) {
+
+    const char *result = path;
+    for (const char *symbol = path; *symbol != '\0' && (symbol - path < 255); ++symbol) {
+
+        const char *next_symbol = symbol + 1;
+
+        if (*next_symbol != '\0' && (*symbol == '\\' || *symbol == '/')) {
+            result = next_symbol;
+        }
+    }
+
+    return result;
+}
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
