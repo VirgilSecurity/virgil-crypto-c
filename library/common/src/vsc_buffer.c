7a895173b8b47dd09c633d2d0b6faac3c63a0927
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-06-11 11:50:41 +0300

    Add project 'common' with classes 'data' and 'buffer'

---
+++ library/common/src/vsc_buffer.c
@@ -0,0 +1,267 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Encapsulates fixed byte array with variable effective data length.
+// --------------------------------------------------------------------------
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+#include "vsc_buffer.h"
+#include "vsc_memory.h"
+#include "vsc_assert.h"
+#include "vsc_buffer_defs.h"
+//  @end
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Allocate context and perform it's initialization.
+//
+VSC_PUBLIC vsc_buffer_t*
+vsc_buffer_new(void) {
+
+    vsc_buffer_t *buffer_ctx = (vsc_buffer_t *) vsc_alloc(sizeof (vsc_buffer_t));
+    if (NULL == buffer_ctx) {
+        return NULL;
+    }
+
+    if (vsc_buffer_init(buffer_ctx) != vsc_SUCCESS) {
+        vsc_dealloc(buffer_ctx);
+        return NULL;
+    }
+
+    buffer_ctx->self_dealloc_cb = vsc_dealloc;
+
+    return buffer_ctx;
+}
+
+//
+//  Release all inner resorces and deallocate context if needed.
+//  It is safe to call this method even if context was allocated by the caller.
+//
+VSC_PUBLIC void
+vsc_buffer_delete(vsc_buffer_t* buffer_ctx) {
+
+    if (NULL == buffer_ctx) {
+        return;
+    }
+
+    vsc_buffer_cleanup(buffer_ctx);
+
+    if (buffer_ctx->self_dealloc_cb != NULL) {
+         buffer_ctx->self_dealloc_cb(buffer_ctx);
+    }
+}
+
+//
+//  Delete given context and nullifies reference.
+//  This is a reverse action of the function 'vsc_buffer_new ()'.
+//
+VSC_PUBLIC void
+vsc_buffer_destroy(vsc_buffer_t** buffer_ctx_ref) {
+
+    VSC_ASSERT_PTR(buffer_ctx_ref);
+
+    vsc_buffer_t *buffer_ctx = *buffer_ctx_ref;
+    *buffer_ctx_ref = NULL;
+
+    vsc_buffer_delete(buffer_ctx);
+}
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
+
+
+//
+//  Perform initialization of pre-allocated context.
+//
+VSC_PUBLIC vsc_error_t
+vsc_buffer_init(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+
+    vsc_zeroize(buffer_ctx, sizeof(vsc_buffer_t));
+
+    return vsc_SUCCESS;
+}
+
+//
+//  Release all inner resources.
+//
+VSC_PUBLIC void
+vsc_buffer_cleanup(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    if (buffer_ctx->bytes != NULL && buffer_ctx->bytes_dealloc_cb != NULL) {
+        buffer_ctx->bytes_dealloc_cb(buffer_ctx->bytes);
+    }
+    buffer_ctx->bytes = NULL;
+    buffer_ctx->capacity = 0;
+    buffer_ctx->len = 0;
+    buffer_ctx->bytes_dealloc_cb = NULL;
+}
+
+//
+//  Allocates inner buffer with a given capacity.
+//  Precondition: buffer is initialized.
+//  Precondition: buffer does not hold any bytes.
+//
+VSC_PUBLIC vsc_error_t
+vsc_buffer_alloc(vsc_buffer_t* buffer_ctx, size_t capacity) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT(capacity > 0);
+    VSC_ASSERT(NULL == buffer_ctx->bytes);
+
+    buffer_ctx->bytes = (byte*)vsc_alloc(capacity);
+    if (NULL == buffer_ctx->bytes) {
+        return vsc_error_NO_MEMORY;
+    }
+
+    buffer_ctx->capacity = capacity;
+    buffer_ctx->len = 0;
+    buffer_ctx->bytes_dealloc_cb = vsc_dealloc;
+
+    return vsc_SUCCESS;
+}
+
+//
+//  Use given data as output buffer.
+//  Client side is responsible for data deallocation.
+//  Precondition: buffer is initialized.
+//  Precondition: buffer does not hold any bytes.
+//
+VSC_PUBLIC void
+vsc_buffer_use(vsc_buffer_t* buffer_ctx, byte* bytes, size_t bytes_len) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(bytes);
+    VSC_ASSERT(bytes_len > 0);
+    VSC_ASSERT(NULL == buffer_ctx->bytes);
+
+    buffer_ctx->bytes = bytes;
+    buffer_ctx->capacity = bytes_len;
+    buffer_ctx->len = 0;
+    buffer_ctx->bytes_dealloc_cb = NULL;
+}
+
+//
+//  Use given data as output buffer.
+//  Buffer is responsible for data deallocation.
+//  Precondition: buffer is initialized.
+//  Precondition: buffer does not hold any bytes.
+//
+VSC_PUBLIC void
+vsc_buffer_take(vsc_buffer_t* buffer_ctx, byte* bytes, size_t bytes_len, vsc_dealloc_fn dealloc_cb) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(bytes);
+    VSC_ASSERT(bytes_len > 0);
+    VSC_ASSERT_PTR(dealloc_cb);
+    VSC_ASSERT(NULL == buffer_ctx->bytes);
+
+    buffer_ctx->bytes = bytes;
+    buffer_ctx->capacity = bytes_len;
+    buffer_ctx->len = 0;
+    buffer_ctx->bytes_dealloc_cb = dealloc_cb;
+}
+
+//
+//  Returns underlying buffer bytes.
+//
+VSC_PUBLIC const byte*
+vsc_buffer_bytes(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(buffer_ctx->bytes);
+
+    return buffer_ctx->bytes;
+}
+
+//
+//  Returns underlying buffer bytes as object.
+//
+VSC_PUBLIC vsc_data_t
+vsc_buffer_data(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(buffer_ctx->bytes);
+
+    return vsc_data(buffer_ctx->bytes, buffer_ctx->len);
+}
+
+//
+//  Returns buffer capacity.
+//
+VSC_PUBLIC size_t
+vsc_buffer_capacity(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(buffer_ctx->bytes);
+
+    return buffer_ctx->capacity;
+}
+
+//
+//  Returns buffer length - length of bytes actually used.
+//
+VSC_PUBLIC size_t
+vsc_buffer_len(vsc_buffer_t* buffer_ctx) {
+
+    VSC_ASSERT_PTR(buffer_ctx);
+    VSC_ASSERT_PTR(buffer_ctx->bytes);
+
+    return buffer_ctx->len;
+}
---
+++ library/common/src/vsc_memory.c
@@ -0,0 +1,165 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Provides configurable memory management model.
+// --------------------------------------------------------------------------
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+#include "vsc_memory.h"
+#include "vsc_assert.h"
+//  @end
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Default allocation function, that is configured during compilation.
+//
+static void*
+vsc_default_alloc(size_t size);
+
+//
+//  Default de-allocation function, that is configured during compilation.
+//
+static void
+vsc_default_dealloc(void* mem);
+
+//
+//  Current allocation function.
+//
+static vsc_alloc_fn inner_alloc = vsc_default_alloc;
+
+//
+//  Current de-allocation function.
+//
+static vsc_dealloc_fn inner_dealloc = vsc_default_dealloc;
+
+//
+//  Default allocation function, that is configured during compilation.
+//
+static void*
+vsc_default_alloc(size_t size) {
+
+    return VSC_ALLOC_DEFAULT (size);
+}
+
+//
+//  Default de-allocation function, that is configured during compilation.
+//
+static void
+vsc_default_dealloc(void* mem) {
+
+    VSC_DEALLOC_DEFAULT (mem);
+}
+
+//
+//  Allocate required amount of memory by usging current allocation function.
+//  Returns NULL if memory allocation fails.
+//
+VSC_PUBLIC void*
+vsc_alloc(size_t size) {
+
+    return inner_alloc (size);
+}
+
+//
+//  Deallocate given memory by usging current de-allocation function.
+//
+VSC_PUBLIC void
+vsc_dealloc(void* mem) {
+
+    inner_dealloc (mem);
+}
+
+//
+//  Change current used memory functions in the runtime.
+//
+VSC_PUBLIC void
+vsc_set_allocators(vsc_alloc_fn alloc_cb, vsc_dealloc_fn dealloc_cb) {
+
+    VSC_ASSERT_PTR (alloc_cb);
+    VSC_ASSERT_PTR (dealloc_cb);
+
+    inner_alloc = alloc_cb;
+    inner_dealloc = dealloc_cb;
+}
+
+//
+//  Zeroize memory.
+//  Note, this function can be reduced by compiler during optimization step.
+//  For sensitive data erasing use vsc_erase ().
+//
+VSC_PUBLIC void
+vsc_zeroize(void* mem, size_t size) {
+
+    VSC_ASSERT_PTR (mem);
+    memset (mem, 0, size);
+}
+
+//
+//  Zeroize memory in a secure manner.
+//  Compiler can not reduce this function during optimization step.
+//
+VSC_PUBLIC void
+vsc_erase(void* mem, size_t size) {
+
+    VSC_ASSERT_PTR (mem);
+
+    volatile uint8_t* p = (uint8_t*)mem;
+    while (size--) { *p++ = 0; }
+}
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
