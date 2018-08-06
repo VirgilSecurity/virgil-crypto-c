7dffa99ffb64f262d5bf2a4ea613e77d36c9a187
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-08-06 19:10:55 +0300

    Fix naming for 'rsa public key' dependencies

--- library/foundation/include/virgil/foundation/vscf_rsa_public_key.h
+++ library/foundation/include/virgil/foundation/vscf_rsa_public_key.h
@@ -149,28 +149,28 @@ VSCF_PUBLIC void
 vscf_rsa_public_key_take_random(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t **random_ref);
 
 //
-//  Setup dependency to the interface 'asn1 writer' and keep ownership.
-//
-VSCF_PUBLIC void
-vscf_rsa_public_key_use_asn1_writer(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t *asn1rd);
-
-//
-//  Setup dependency to the interface 'asn1 writer' and transfer ownership.
-//
-VSCF_PUBLIC void
-vscf_rsa_public_key_take_asn1_writer(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t **asn1rd_ref);
-
-//
 //  Setup dependency to the interface 'asn1 reader' and keep ownership.
 //
 VSCF_PUBLIC void
-vscf_rsa_public_key_use_asn1_reader(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t *asn1wr);
+vscf_rsa_public_key_use_asn1_reader(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t *asn1rd);
 
 //
 //  Setup dependency to the interface 'asn1 reader' and transfer ownership.
 //
 VSCF_PUBLIC void
-vscf_rsa_public_key_take_asn1_reader(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t **asn1wr_ref);
+vscf_rsa_public_key_take_asn1_reader(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t **asn1rd_ref);
+
+//
+//  Setup dependency to the interface 'asn1 writer' and keep ownership.
+//
+VSCF_PUBLIC void
+vscf_rsa_public_key_use_asn1_writer(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t *asn1wr);
+
+//
+//  Setup dependency to the interface 'asn1 writer' and transfer ownership.
+//
+VSCF_PUBLIC void
+vscf_rsa_public_key_take_asn1_writer(vscf_rsa_public_key_impl_t *rsa_public_key_impl, vscf_impl_t **asn1wr_ref);
 
 //
 //  Returns instance of the implemented interface 'public key'.
