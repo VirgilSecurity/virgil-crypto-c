56eb931e60f26c7769b7226e0cc515c1e9435a50
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-08-13 17:40:59 +0300

    Regenerate code

--- library/common/src/vsc_data.c
+++ library/common/src/vsc_data.c
@@ -61,15 +61,6 @@
 //  Generated section start.
 // --------------------------------------------------------------------------
 
-//
-//  Return size of 'vsc_data_t'.
-//
-VSC_PUBLIC size_t
-vsc_data_ctx_size(void) {
-
-    return sizeof(vsc_data_t);
-}
-
 
 // --------------------------------------------------------------------------
 //  Generated section end.
