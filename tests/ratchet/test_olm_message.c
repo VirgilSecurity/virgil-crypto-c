797724056cb305f731eb4ef6b09681ea084c1bcb
Oleksandr Deundiak <Deundiak@gmail.com>
2018-09-06 16:02:37 +0300

    Fixed OLMMessage + added tests

---
+++ tests/ratchet/test_olm_message.c
@@ -0,0 +1,144 @@
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+
+#include <virgil/common/private/vsc_buffer_defs.h>
+#include "unity.h"
+#include "test_utils.h"
+
+#define TEST_DEPENDENCIES_AVAILABLE VSCR_OLM_MESSAGE
+#if TEST_DEPENDENCIES_AVAILABLE
+
+#include "vscr_olm_message.h"
+#include "test_data_olm_message.h"
+
+void
+test__1(void) {
+    vsc_buffer_t *public_key = vsc_buffer_new_with_capacity(test_olm_message_public_key.len);
+    memcpy(vsc_buffer_ptr(public_key), test_olm_message_public_key.bytes, test_olm_message_public_key.len);
+    vsc_buffer_reserve(public_key, test_olm_message_public_key.len);
+
+    vsc_buffer_t *cipher_text = vsc_buffer_new_with_capacity(test_olm_message_cipher_text.len);
+    memcpy(vsc_buffer_ptr(cipher_text), test_olm_message_cipher_text.bytes, test_olm_message_cipher_text.len);
+    vsc_buffer_reserve(cipher_text, test_olm_message_cipher_text.len);
+
+    vscr_olm_message_t *msg1 = vscr_olm_message_new_with_members(test_olm_message_version, test_olm_message_counter,
+                                                                 &public_key, &cipher_text);
+
+    TEST_ASSERT_EQUAL_INT(test_olm_message_version, msg1->version);
+    TEST_ASSERT_EQUAL_INT(test_olm_message_counter, msg1->counter);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->public_key->bytes, test_olm_message_public_key.bytes, test_olm_message_public_key.len);
+    TEST_ASSERT_EQUAL_INT(32, vsc_buffer_len(msg1->public_key));
+    TEST_ASSERT_EQUAL_MEMORY(msg1->cipher_text->bytes, test_olm_message_cipher_text.bytes, test_olm_message_cipher_text.len);
+    TEST_ASSERT_EQUAL_INT(test_olm_message_cipher_text.len, vsc_buffer_len(msg1->cipher_text));
+
+    size_t len = vscr_olm_message_serialize_len(msg1);
+    vsc_buffer_t *buffer = vsc_buffer_new_with_capacity(len);
+
+    TEST_ASSERT_EQUAL(vscr_olm_message_serialize(msg1, buffer), vscr_SUCCESS);
+
+    vscr_error_ctx_t err_ctx;
+    vscr_error_ctx_reset(&err_ctx);
+    vscr_olm_message_t *msg2 = vscr_olm_message_deserialize(vsc_buffer_data(buffer), &err_ctx);
+
+    TEST_ASSERT_EQUAL(err_ctx.error, vscr_SUCCESS);
+
+    TEST_ASSERT_EQUAL_INT(msg1->public_key->len, msg2->public_key->len);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->public_key->bytes, msg2->public_key->bytes, msg1->public_key->len);
+    TEST_ASSERT_EQUAL_INT(msg1->cipher_text->len, msg2->cipher_text->len);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->cipher_text->bytes, msg2->cipher_text->bytes, msg1->cipher_text->len);
+
+    vscr_olm_message_destroy(&msg1);
+    vscr_olm_message_destroy(&msg2);
+}
+
+void
+test__2(void) {
+    vsc_buffer_t *public_key = vsc_buffer_new_with_capacity(test_olm_message_public_key.len);
+    memcpy(vsc_buffer_ptr(public_key), test_olm_message_public_key.bytes, test_olm_message_public_key.len);
+    vsc_buffer_reserve(public_key, test_olm_message_public_key.len);
+
+    vsc_buffer_t *cipher_text = vsc_buffer_new_with_capacity(test_olm_message_cipher_text_big.len);
+    memcpy(vsc_buffer_ptr(cipher_text), test_olm_message_cipher_text_big.bytes, test_olm_message_cipher_text_big.len);
+    vsc_buffer_reserve(cipher_text, test_olm_message_cipher_text_big.len);
+
+    vscr_olm_message_t *msg1 = vscr_olm_message_new_with_members(test_olm_message_version_big,
+                                                                 test_olm_message_counter_big,
+                                                                 &public_key, &cipher_text);
+
+    TEST_ASSERT_EQUAL_INT(test_olm_message_version_big, msg1->version);
+    TEST_ASSERT_EQUAL_INT(test_olm_message_counter_big, msg1->counter);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->public_key->bytes, test_olm_message_public_key.bytes, test_olm_message_public_key.len);
+    TEST_ASSERT_EQUAL_INT(32, vsc_buffer_len(msg1->public_key));
+    TEST_ASSERT_EQUAL_MEMORY(msg1->cipher_text->bytes, test_olm_message_cipher_text_big.bytes, test_olm_message_cipher_text_big.len);
+    TEST_ASSERT_EQUAL_INT(test_olm_message_cipher_text_big.len, vsc_buffer_len(msg1->cipher_text));
+
+    size_t len = vscr_olm_message_serialize_len(msg1);
+    vsc_buffer_t *buffer = vsc_buffer_new_with_capacity(len);
+
+    TEST_ASSERT_EQUAL(vscr_olm_message_serialize(msg1, buffer), vscr_SUCCESS);
+
+    vscr_error_ctx_t err_ctx;
+    vscr_error_ctx_reset(&err_ctx);
+    vscr_olm_message_t *msg2 = vscr_olm_message_deserialize(vsc_buffer_data(buffer), &err_ctx);
+
+    TEST_ASSERT_EQUAL(err_ctx.error, vscr_SUCCESS);
+
+    TEST_ASSERT_EQUAL_INT(msg1->public_key->len, msg2->public_key->len);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->public_key->bytes, msg2->public_key->bytes, msg1->public_key->len);
+    TEST_ASSERT_EQUAL_INT(msg1->cipher_text->len, msg2->cipher_text->len);
+    TEST_ASSERT_EQUAL_MEMORY(msg1->cipher_text->bytes, msg2->cipher_text->bytes, msg1->cipher_text->len);
+
+    vscr_olm_message_destroy(&msg1);
+    vscr_olm_message_destroy(&msg2);
+}
+
+#endif // TEST_DEPENDENCIES_AVAILABLE
+
+
+// --------------------------------------------------------------------------
+// Entrypoint.
+// --------------------------------------------------------------------------
+int
+main(void) {
+    UNITY_BEGIN();
+
+#if TEST_DEPENDENCIES_AVAILABLE
+    RUN_TEST(test__1);
+    RUN_TEST(test__2);
+#else
+    RUN_TEST(test__nothing__feature_disabled__must_be_ignored);
+#endif
+
+    return UNITY_END();
+}
