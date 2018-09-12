a5295f83bc90b2a1ec78bd597dd145961c1884f5
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-09-12 11:20:20 +0300

    Beautify CMake files (build)

--- thirdparty/ed25519/definitions.cmake
+++ thirdparty/ed25519/definitions.cmake
@@ -34,6 +34,7 @@
 #
 #   Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
 #   -------------------------------------------------------------------------
+#   @end
 
 
 include_guard()
