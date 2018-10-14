8b5d35ef0c5fbd9244e14a71cb32bde908193030
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-10-14 20:13:39 +0300

    Add skeleton Xcode projects (swift, build)

---
+++ wrappers/swift/VirgilCrypto/VirgilCryptoPythia/VirgilCryptoPythia.h
@@ -0,0 +1,19 @@
+//
+//  VirgilCryptoPythia.h
+//  VirgilCryptoPythia
+//
+//  Created by Sergey Seroshtan on 10/14/18.
+//  Copyright © 2018 Virgil Security, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirgilCryptoPythia.
+FOUNDATION_EXPORT double VirgilCryptoPythiaVersionNumber;
+
+//! Project version string for VirgilCryptoPythia.
+FOUNDATION_EXPORT const unsigned char VirgilCryptoPythiaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirgilCryptoPythia/PublicHeader.h>
+
+
