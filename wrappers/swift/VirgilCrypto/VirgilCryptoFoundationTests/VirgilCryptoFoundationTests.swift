d61b60d2c8c5dbf58105d6ba6a2bedcfea3cfff1
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-10-24 16:49:07 +0300

    Add Swift test test_Sha512_hash_helloString_success()' (swift)

--- wrappers/swift/VirgilCrypto/VirgilCryptoFoundationTests/VirgilCryptoFoundationTests.swift
+++ wrappers/swift/VirgilCrypto/VirgilCryptoFoundationTests/VirgilCryptoFoundationTests.swift
@@ -21,6 +21,11 @@ class VirgilCryptoFoundationTests: XCTestCase {
         XCTAssert("47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=" == hash.base64EncodedString())
     }
 
+    func test_Sha512_hash_helloString_success() {
+        let hash = Sha512().hash(data: "hello".data(using: .utf8)!)
+        XCTAssert("m3HSJL1i83hdltRq0+o9czGb+8KJDKra4t/3JRlnPKcjI8PZm6XBHXx6zG4UuMXaDEZjR1wuXDre9G9zvN7AQw==" == hash.base64EncodedString())
+    }
+    
     func test_Hkdf_derive_emptyString_success() {
         let hkdf = Hkdf()
         hkdf.setHash(hash: Sha256())
