4bd69348313a51bef51f3c74ba62ee6f89fe8f1d
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-10-29 23:55:08 +0200

    Add Apple binaries for the all platforms (bin)

---
+++ wrappers/swift/VirgilCrypto/Binaries/macOS/VSCFoundation.framework/Versions/A/Headers/vscf_api.h
@@ -0,0 +1,133 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+// clang-format off
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Create module with functionality common for all 'api' objects.
+//  It is also enumerate all available interfaces within crypto libary.
+// --------------------------------------------------------------------------
+
+#ifndef VSCF_API_H_INCLUDED
+#define VSCF_API_H_INCLUDED
+
+#include "vscf_library.h"
+
+// clang-format on
+//  @end
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Enumerates all possible interfaces within crypto library.
+//
+enum vscf_api_tag_t {
+    vscf_api_tag_BEGIN = 0,
+    vscf_api_tag_ASN1_READER,
+    vscf_api_tag_ASN1_WRITER,
+    vscf_api_tag_AUTH_DECRYPT,
+    vscf_api_tag_AUTH_ENCRYPT,
+    vscf_api_tag_CIPHER,
+    vscf_api_tag_CIPHER_AUTH,
+    vscf_api_tag_CIPHER_AUTH_INFO,
+    vscf_api_tag_CIPHER_INFO,
+    vscf_api_tag_COMPUTE_SHARED_KEY,
+    vscf_api_tag_DECRYPT,
+    vscf_api_tag_ENCRYPT,
+    vscf_api_tag_EX_KDF,
+    vscf_api_tag_EXPORT_PRIVATE_KEY,
+    vscf_api_tag_EXPORT_PUBLIC_KEY,
+    vscf_api_tag_GENERATE_KEY,
+    vscf_api_tag_HASH,
+    vscf_api_tag_HASH_INFO,
+    vscf_api_tag_HASH_STREAM,
+    vscf_api_tag_IMPORT_PRIVATE_KEY,
+    vscf_api_tag_IMPORT_PUBLIC_KEY,
+    vscf_api_tag_KDF,
+    vscf_api_tag_KEY,
+    vscf_api_tag_MAC,
+    vscf_api_tag_MAC_INFO,
+    vscf_api_tag_MAC_STREAM,
+    vscf_api_tag_PRIVATE_KEY,
+    vscf_api_tag_PUBLIC_KEY,
+    vscf_api_tag_RANDOM,
+    vscf_api_tag_SIGN,
+    vscf_api_tag_VERIFY,
+    vscf_api_tag_END
+};
+typedef enum vscf_api_tag_t vscf_api_tag_t;
+
+//
+//  Generic type for any 'API' object.
+//
+typedef struct vscf_api_t vscf_api_t;
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+//  @footer
+#endif // VSCF_API_H_INCLUDED
+//  @end
---
+++ wrappers/swift/VirgilCrypto/Binaries/watchOS/VSCFoundation.framework/Headers/vscf_compute_shared_key.h
@@ -0,0 +1,142 @@
+//  @license
+// --------------------------------------------------------------------------
+//  Copyright (C) 2015-2018 Virgil Security Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// --------------------------------------------------------------------------
+// clang-format off
+
+
+//  @warning
+// --------------------------------------------------------------------------
+//  This file is partially generated.
+//  Generated blocks are enclosed between tags [@<tag>, @end].
+//  User's code can be added between tags [@end, @<tag>].
+// --------------------------------------------------------------------------
+
+
+//  @description
+// --------------------------------------------------------------------------
+//  Provide interface to compute shared key for 2 asymmetric keys.
+//  Assume that this interface is implemented on the private key.
+// --------------------------------------------------------------------------
+
+#ifndef VSCF_COMPUTE_SHARED_KEY_H_INCLUDED
+#define VSCF_COMPUTE_SHARED_KEY_H_INCLUDED
+
+#include "vscf_library.h"
+#include "vscf_impl.h"
+#include "vscf_error.h"
+#include "vscf_api.h"
+
+#if !VSCF_IMPORT_PROJECT_COMMON_FROM_FRAMEWORK
+#   include <virgil/crypto/common/vsc_buffer.h>
+#endif
+
+#if VSCF_IMPORT_PROJECT_COMMON_FROM_FRAMEWORK
+#   include <VSCCommon/vsc_buffer.h>
+#endif
+
+// clang-format on
+//  @end
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//  @generated
+// --------------------------------------------------------------------------
+// clang-format off
+//  Generated section start.
+// --------------------------------------------------------------------------
+
+//
+//  Contains API requirements of the interface 'compute shared key'.
+//
+typedef struct vscf_compute_shared_key_api_t vscf_compute_shared_key_api_t;
+
+//
+//  Compute shared key for 2 asymmetric keys.
+//  Note, shared key can be used only for symmetric cryptography.
+//
+VSCF_PUBLIC vscf_error_t
+vscf_compute_shared_key(vscf_impl_t *impl, const vscf_impl_t *public_key, vsc_buffer_t *shared_key);
+
+//
+//  Return number of bytes required to hold shared key.
+//
+VSCF_PUBLIC size_t
+vscf_compute_shared_key_shared_key_len(vscf_impl_t *impl);
+
+//
+//  Return compute shared key API, or NULL if it is not implemented.
+//
+VSCF_PUBLIC const vscf_compute_shared_key_api_t *
+vscf_compute_shared_key_api(vscf_impl_t *impl);
+
+//
+//  Check if given object implements interface 'compute shared key'.
+//
+VSCF_PUBLIC bool
+vscf_compute_shared_key_is_implemented(vscf_impl_t *impl);
+
+//
+//  Returns interface unique identifier.
+//
+VSCF_PUBLIC vscf_api_tag_t
+vscf_compute_shared_key_api_tag(const vscf_compute_shared_key_api_t *compute_shared_key_api);
+
+//
+//  Returns implementation unique identifier.
+//
+VSCF_PUBLIC vscf_impl_tag_t
+vscf_compute_shared_key_impl_tag(const vscf_compute_shared_key_api_t *compute_shared_key_api);
+
+
+// --------------------------------------------------------------------------
+//  Generated section end.
+// clang-format on
+// --------------------------------------------------------------------------
+//  @end
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+//  @footer
+#endif // VSCF_COMPUTE_SHARED_KEY_H_INCLUDED
+//  @end
