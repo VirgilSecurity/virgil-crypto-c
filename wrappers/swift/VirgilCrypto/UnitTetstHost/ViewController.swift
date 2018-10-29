8afe865762f28c394f8df6d689969f8eb257de62
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-10-29 18:32:39 +0200

    Add Swift iOS application for tests purposes (swift, xcode)

---
+++ wrappers/swift/VirgilCrypto/UnitTetstHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTetstHost
+//
+//  Created by Sergey Seroshtan on 10/29/18.
+//  Copyright © 2018 Virgil Security, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
