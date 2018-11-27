3a8608b07c242a93fba0029aa512ae7547b1e1c7
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-11-27 16:31:28 +0200

    Add methods "is_empty()" and "equal" to the class "data" (models, lib)

--- library/common/src/vsc_data.c
+++ library/common/src/vsc_data.c
@@ -146,6 +146,32 @@ vsc_data_is_zero(vsc_data_t data_ctx) {
 }
 
 //
+//  Returns true if underlying byte array is empty.
+//
+VSC_PUBLIC bool
+vsc_data_is_empty(vsc_data_t data_ctx) {
+
+    return 0 == data_ctx.len;
+}
+
+//
+//  Return true if given datas are equal.
+//
+VSC_PUBLIC bool
+vsc_data_equal(vsc_data_t data_ctx, vsc_data_t rhs) {
+
+    VSC_ASSERT(vsc_data_is_valid(data_ctx));
+    VSC_ASSERT(vsc_data_is_valid(rhs));
+
+    if (data_ctx.len != rhs.len) {
+        return false;
+    }
+
+    bool is_equal = memcmp(data_ctx.bytes, rhs.bytes, rhs.len) == 0;
+    return is_equal;
+}
+
+//
 //  Return underlying data slice starting from beginning.
 //
 VSC_PUBLIC vsc_data_t
