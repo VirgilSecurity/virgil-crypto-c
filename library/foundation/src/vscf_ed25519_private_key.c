b0a90d6d71105ca5fc3e6c0c1a43435f046bc070
Victor <vtatisch@gmail.com>
2018-12-11 17:38:58 +0300

    Fix memory leak in ed25519 private key. Update code.

--- library/foundation/src/vscf_ed25519_private_key.c
+++ library/foundation/src/vscf_ed25519_private_key.c
@@ -185,7 +185,6 @@ vscf_ed25519_private_key_extract_public_key(vscf_ed25519_private_key_impl_t *ed2
 
     VSCF_ASSERT_PTR(ed25519_private_key_impl);
     vscf_ed25519_public_key_impl_t *ed25519_public_key_impl = vscf_ed25519_public_key_new();
-    VSCF_ASSERT_ALLOC(ed25519_public_key_impl != NULL);
     int ret = ed25519_get_pubkey(ed25519_public_key_impl->public_key, ed25519_private_key_impl->secret_key);
     VSCF_ASSERT(ret == 0);
     vscf_ed25519_public_key_impl_t *ed25519_public_key_le_impl = vscf_ed25519_public_key_new();
@@ -193,6 +192,7 @@ vscf_ed25519_private_key_extract_public_key(vscf_ed25519_private_key_impl_t *ed2
     vsc_buffer_use(dst, ed25519_public_key_le_impl->public_key, ED25519_KEY_LEN);
     vscf_endianness_reverse_memcpy(vsc_data(ed25519_public_key_impl->public_key, ED25519_KEY_LEN), dst);
     vsc_buffer_destroy(&dst);
+    vscf_ed25519_public_key_delete(ed25519_public_key_impl);
     return vscf_ed25519_public_key_impl(ed25519_public_key_le_impl);
 }
 
@@ -263,8 +263,10 @@ vscf_ed25519_private_key_compute_shared_key(vscf_ed25519_private_key_impl_t *ed2
     VSCF_ASSERT_PTR(available >= ED25519_KEY_LEN);
     int ret = curve25519_key_exchange(ptr, ed25519_public_key_impl->public_key, ed25519_private_key_impl->secret_key);
     if (!ret) {
+        vscf_ed25519_public_key_delete(ed25519_public_key_impl);
         return vscf_SUCCESS;
     }
+    vscf_ed25519_public_key_delete(ed25519_public_key_impl);
     return vscf_error_SHARED_KEY_EXCHANGE_FAILED;
 }
 
