d55795a74a97bd5eb6b3fbea6fadb1789cc32cae
SergeySeroshtan <sergey.seroshtan@gmail.com>
2018-12-13 15:33:32 +0200

    Switch of code formatting for test utility functions (tests)

--- tests/foundation/test_hkdf.c
+++ tests/foundation/test_hkdf.c
@@ -54,10 +54,12 @@
 // --------------------------------------------------------------------------
 //  Should have it to prevent linkage erros in MSVC.
 // --------------------------------------------------------------------------
+// clang-format off
 void setUp(void) { }
 void tearDown(void) { }
 void suiteSetUp(void) { }
 int suiteTearDown(int num_failures) { return num_failures; }
+// clang-format on
 
 
 // --------------------------------------------------------------------------
