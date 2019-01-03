bb59759a36055ae4b9163c0d5137db1e969e8f0b
SergeySeroshtan <sergey.seroshtan@gmail.com>
2019-01-03 18:26:20 +0200

    Mark compile definition 'VSC*_INTERNAL_BUILD' as public (cmake, codegen)

--- library/pythia/definitions.cmake
+++ library/pythia/definitions.cmake
@@ -51,10 +51,10 @@ endif()
 
 target_compile_definitions(pythia_wrap
         PUBLIC
+            $<BUILD_INTERFACE:VSCP_INTERNAL_BUILD>
             "VSCP_LIBRARY=$<BOOL:${VSCP_LIBRARY}>"
             "VSCP_MULTI_THREAD=$<BOOL:${VSCP_MULTI_THREAD}>"
             "VSCP_PYTHIA=$<BOOL:${VSCP_PYTHIA}>"
         PRIVATE
             $<$<BOOL:${BUILD_SHARED_LIBS}>:VSCP_BUILD_SHARED_LIBS>
-            $<BUILD_INTERFACE:VSCP_INTERNAL_BUILD>
         )
