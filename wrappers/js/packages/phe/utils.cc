9a6897de9e61e036c37ab40800daa57530556717
Serhii Nanovskyi <s.nanovskyi@gmail.com>
2019-01-21 14:09:19 +0200

    Fix 'utils::VirgilBufferToNodeBuffer'

--- wrappers/js/packages/phe/utils.cc
+++ wrappers/js/packages/phe/utils.cc
@@ -1,10 +1,10 @@
 #include "utils.h"
 
 Nan::MaybeLocal<v8::Object> utils::VirgilBufferToNodeBuffer(vsc_buffer_t* buffer) {
-  size_t capacity = vsc_buffer_capacity(buffer);
-  char* data = new char[capacity];
-  std::memcpy(data, vsc_buffer_bytes(buffer), capacity);
-  return Nan::NewBuffer(data, capacity);
+  size_t buffer_len = vsc_buffer_len(buffer);
+  char* data = new char[buffer_len];
+  std::memcpy(data, vsc_buffer_bytes(buffer), buffer_len);
+  return Nan::NewBuffer(data, buffer_len);
 }
 
 vsc_data_t utils::NodeBufferToVirgilData(v8::Local<v8::Value> value) {
