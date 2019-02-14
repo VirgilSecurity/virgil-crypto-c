b4460a68bacec4f9c163aa1bbdcbcc804a3cfbaa
SergeySeroshtan <sergey.seroshtan@gmail.com>
2019-02-14 13:35:56 +0200

    Fix rsa keys exporting (lib, foundation)

--- library/foundation/src/vscf_rsa_public_key.c
+++ library/foundation/src/vscf_rsa_public_key.c
@@ -338,8 +338,11 @@ vscf_rsa_public_key_export_public_key(const vscf_rsa_public_key_t *self, vsc_buf
     VSCF_ASSERT(vscf_asn1_writer_error(self->asn1wr) == vscf_SUCCESS);
     VSCF_ASSERT(error.error == vscf_SUCCESS);
 
-    vscf_asn1_writer_finish(self->asn1wr);
     vsc_buffer_inc_used(out, len);
+
+    if (!vsc_buffer_is_reverse(out)) {
+        vscf_asn1_writer_finish(self->asn1wr);
+    }
 
     return vscf_SUCCESS;
 }
