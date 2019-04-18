82306206a195ff9e7943c81d38545bea0acd79a7
SergeySeroshtan <sergey.seroshtan@gmail.com>
2019-04-18 16:54:59 +0300

    Bump version to the v0.7.0-alpha2

--- library/common/include/virgil/crypto/common/vsc_library.h
+++ library/common/include/virgil/crypto/common/vsc_library.h
@@ -115,9 +115,9 @@ extern "C" {
 
 #define VSC_VERSION_MAJOR 0
 
-#define VSC_VERSION_MINOR 6
+#define VSC_VERSION_MINOR 7
 
-#define VSC_VERSION_PATCH 1
+#define VSC_VERSION_PATCH 0
 
 #define VSC_VERSION_MAKE(major, minor, patch) ((major) * 10000 + (minor) * 100 + (patch))
 
