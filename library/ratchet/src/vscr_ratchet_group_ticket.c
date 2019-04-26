e0256d8d1d78d4cd3889807aeb87bb96cbfa7ac9
Oleksandr Deundiak <deundiak@gmail.com>
2019-04-26 15:37:15 +0300

    Memcpy warning fix

--- library/ratchet/src/vscr_ratchet_group_ticket.c
+++ library/ratchet/src/vscr_ratchet_group_ticket.c
@@ -500,8 +500,11 @@ vscr_ratchet_group_ticket_remove_participant(vscr_ratchet_group_ticket_t *self, 
         return vscr_status_ERROR_PARTICIPANT_NOT_FOUND;
     }
 
-    memcpy(&msg_info->participants[i], &msg_info->participants[msg_info->participants_count - 1],
-            sizeof(MessageParticipantInfo));
+    if (i != msg_info->participants_count - 1) {
+        memcpy(&msg_info->participants[i], &msg_info->participants[msg_info->participants_count - 1],
+               sizeof(MessageParticipantInfo));
+    }
+    
     vscr_zeroize(&msg_info->participants[msg_info->participants_count - 1], sizeof(MessageParticipantInfo));
     msg_info->participants_count--;
 
