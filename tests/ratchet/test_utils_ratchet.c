14e78cea667a2d59b4690ed702978a5b35623b93
Oleksandr Deundiak <deundiak@gmail.com>
2019-06-15 00:22:14 +0300

    Fixed memory leak in tests

--- tests/ratchet/test_utils_ratchet.c
+++ tests/ratchet/test_utils_ratchet.c
@@ -675,6 +675,9 @@ initialize_random_group_chat(vscf_ctr_drbg_t *rng, size_t group_size, vscr_ratch
     vscr_ratchet_group_ticket_use_rng(ticket, vscf_ctr_drbg_impl(rng));
     TEST_ASSERT_EQUAL(
             vscr_status_SUCCESS, vscr_ratchet_group_ticket_setup_ticket_as_new(ticket, vsc_buffer_data(session_id)));
+
+    vsc_buffer_destroy(&session_id);
+
     const vscr_ratchet_group_message_t *msg_start = vscr_ratchet_group_ticket_get_ticket_message(ticket);
 
     for (size_t i = 0; i < group_size; i++) {
