8c01feda92f095a3b05c2de92e9f8918d523b244
SergeySeroshtan <sergey.seroshtan@gmail.com>
2019-06-20 15:02:00 +0300

    Fix module "atomic" for windows #3 (lib, all)

--- library/foundation/include/virgil/crypto/foundation/private/vscf_atomic.h
+++ library/foundation/include/virgil/crypto/foundation/private/vscf_atomic.h
@@ -70,6 +70,20 @@ extern "C" {
 //  Generated section start.
 // --------------------------------------------------------------------------
 
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#   pragma intrinsic(_InterlockedCompareExchange)
+    inline bool vscf_atomic_compare_exchange_weak(volatile long *obj, long* expected, long desired) {
+        const long expected_local = *expected;
+        const long old = _InterlockedCompareExchange(obj, desired, expected_local);
+        if (old == expected_local) {
+            return true;
+        } else {
+            *expected = old;
+            return false;
+        }
+    }
+#endif
+
 #if VSCF_MULTI_THREADING
 #   if VSCF_HAVE_STDATOMIC_H && !defined(__STDC_NO_ATOMICS__)
 #       define VSCF_ATOMIC _Atomic
@@ -77,18 +91,6 @@ extern "C" {
 #   elif defined(__GNUC__) || defined(__clang__)
 #       define VSCF_ATOMIC_COMPARE_EXCHANGE_WEAK(obj, expected, desired) __atomic_compare_exchange_n(obj, expected, desired, 1, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
 #   elif defined(_MSC_VER) && !defined(__INTEL_COMPILER)
-#       pragma intrinsic(_InterlockedCompareExchange)                                                                                                               \
-                                                                                                                                                                    \
-        inline bool vscf_atomic_compare_exchange_weak(volatile long *obj, long* expected, long desired) {                                                           \
-            const long expected_local = *expected;                                                                                                                  \
-            const long old = _InterlockedCompareExchange(obj, desired, expected_local);                                                                             \
-            if (old == expected_local) {                                                                                                                            \
-                return true;                                                                                                                                        \
-            } else {                                                                                                                                                \
-                *expected = old;                                                                                                                                    \
-                return false;                                                                                                                                       \
-            }                                                                                                                                                       \
-        }
 #       define VSCF_ATOMIC_COMPARE_EXCHANGE_WEAK(obj, expected, desired) vscf_atomic_compare_exchange_weak(obj, expected, desired)
 #   else
 #       error "Atomic operations are not suppored for this platform, but CMake option VSCF_MULTI_THREADING is ON."
