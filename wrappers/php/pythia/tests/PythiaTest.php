bff5e8de88d42fa3c22e35a85b14d8f78070abef
Petrenko Anton <antptrnk@gmail.com>
2019-10-30 13:13:52 +0200

    Add Pythia tests (php, gsl, codegen)

--- wrappers/php/pythia/tests/PythiaTest.php
+++ wrappers/php/pythia/tests/PythiaTest.php
@@ -41,33 +41,62 @@ use VirgilCrypto\Pythia\Pythia;
 
 class PythiaTest extends \PHPUnit\Framework\TestCase
 {
-    protected $pythia;
-    private $password;
+    private $pythia;
+
+    private $kDeblindedPassword;
+
+    private $kPassword;
+    private $kTransformationKeyId;
+    private $kTweak;
+    private $kPythiaSecret;
+    private $kNewPythiaSecret;
+    private $kPythiaScopeSecret;
+    private $kNewPythiaScopeSecret;
 
     protected function setUp()
     {
-        $this->password = "passw0rd";
         $this->pythia = new Pythia();
+        Pythia::configure();
+
+        $this->kDeblindedPassword = "13273238e3119262f86d3213b8eb6b99c093ef48737dfcfae96210f7350e096cbc7e6b992e4e6f705ac3f0a915d1622c1644596408e3d16126ddfa9ce594e9f361b21ef9c82309e5714c09bcd7f7ec5c2666591134c645d45ed8c9703e718ee005fe4b97fc40f69b424728831d0a889cd39be04683dd380daa0df67c38279e3b9fe32f6c40780311f2dfbb6e89fc90ef15fb2c7958e387182dc7ef57f716fdd152a58ac1d3f0d19bfa2f789024333976c69fbe9e24b58d6cd8fa49c5f4d642b00f8e390c199f37f7b3125758ef284ae10fd9c2da7ea280550baccd55dadd70873a063bcfc9cac9079042af88a543a6cc09aaed6ba4954d6ee8ccc6e1145944328266616cd00f8a616f0e79e52ddd2ef970c8ba8f8ffce35505dc643c8e2b6e430a1474a6d043a4daf9b62af87c1d45ca994d23f908f7898a3f44ca7bb642122087ca819308b3d8afad17ca1f6148e8750870336ca68eb783c89b0dc9d92392f453c650e9f09232b9fcffd1c2cad24b14d2b4952b7f54552295ce0e854996913c";
+
+        $this->kPassword = "password";
+        $this->kTransformationKeyId = "virgil.com";
+        $this->kTweak = "alice";
+        $this->kPythiaSecret = "master secret";
+        $this->kNewPythiaSecret = "new master secret";
+        $this->kPythiaScopeSecret = "server secret";
+        $this->kNewPythiaScopeSecret = "new server secret";
     }
 
     protected function tearDown()
     {
+        Pythia::cleanup();
         unset($this->pythia);
     }
 
-    public function test_Pythia_blindValidArgs_returnsSuccess()
+    public function test_Pythia_blindDeblind_returnsSuccess()
     {
         try {
-            Pythia::configure();
-
-            list($blindedPassword, $blindingSecret) = Pythia::blind($this->password);
+            list($blindedPassword, $blindingSecret) = Pythia::blind($this->kPassword);
             $this->assertNotNull($blindedPassword);
             $this->assertNotNull($blindingSecret);
 
-            Pythia::cleanup();
+            list($transformationPrivateKey, $transformationPublicKey) = Pythia::computeTransformationKeyPair
+            ($this->kTransformationKeyId, $this->kPythiaSecret, $this->kPythiaScopeSecret);
 
+            list($transformedPassword, $transformedTweak) = Pythia::transform($blindedPassword, $this->kTweak,
+                $transformationPrivateKey);
+
+            $deblind = Pythia::deblind($transformedPassword, $blindingSecret);
+            $this->assertEquals($this->kDeblindedPassword, unpack("H*", $deblind)[1]);
         } catch (\Exception $e) {
             $this->expectException(\Exception::class);
         }
+    }
+
+    public function test_Pythia_proveVerify_returnsSuccess()
+    {
+
     }
 }
