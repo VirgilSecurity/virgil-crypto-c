bcb264bb58e45e333f706283803906905bbf29ea
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-01-10 18:29:04 +0200

    Bump version to the v0.12.1-dev1 [ci skip]

--- library/pythia/include/virgil/crypto/pythia/vscp_library.h
+++ library/pythia/include/virgil/crypto/pythia/vscp_library.h
@@ -118,7 +118,7 @@ extern "C" {
 
 #define VSCP_VERSION_MINOR 12
 
-#define VSCP_VERSION_PATCH 0
+#define VSCP_VERSION_PATCH 1
 
 #define VSCP_VERSION_MAKE(major, minor, patch) ((major) * 10000 + (minor) * 100 + (patch))
 
