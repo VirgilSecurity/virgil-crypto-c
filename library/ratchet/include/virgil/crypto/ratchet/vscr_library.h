fab080238c447739319a1d063b5a3dc8d1065421
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-02-17 17:16:51 +0200

    Bump version to the v0.12.3-dev1 [ci skip]

--- library/ratchet/include/virgil/crypto/ratchet/vscr_library.h
+++ library/ratchet/include/virgil/crypto/ratchet/vscr_library.h
@@ -118,7 +118,7 @@ extern "C" {
 
 #define VSCR_VERSION_MINOR 12
 
-#define VSCR_VERSION_PATCH 2
+#define VSCR_VERSION_PATCH 3
 
 #define VSCR_VERSION_MAKE(major, minor, patch) ((major) * 10000 + (minor) * 100 + (patch))
 
