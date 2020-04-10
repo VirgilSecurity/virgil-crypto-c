97d7e0878ce016de0153b331c8b12e87d30e7ba8
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-10 09:29:38 +0300

    Remove methods with a scope="private" from the Go wrapper

--- wrappers/go/foundation/falcon.go
+++ wrappers/go/foundation/falcon.go
@@ -231,28 +231,6 @@ func (obj *Falcon) ImportPublicKey(rawKey *RawPublicKey) (PublicKey, error) {
 }
 
 /*
-* Import public key from the raw binary format.
-*/
-func (obj *Falcon) ImportPublicKeyData(keyData []byte, keyAlgInfo AlgInfo) (PublicKey, error) {
-    var error C.vscf_error_t
-    C.vscf_error_reset(&error)
-    keyDataData := helperWrapData (keyData)
-
-    proxyResult := /*pr4*/C.vscf_falcon_import_public_key_data(obj.cCtx, keyDataData, (*C.vscf_impl_t)(unsafe.Pointer(keyAlgInfo.Ctx())), &error)
-
-    err := FoundationErrorHandleStatus(error.status)
-    if err != nil {
-        return nil, err
-    }
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(keyAlgInfo)
-
-    return FoundationImplementationWrapPublicKey(proxyResult) /* r4 */
-}
-
-/*
 * Export public key to the raw binary format.
 *
 * Binary format must be defined in the key specification.
@@ -275,48 +253,6 @@ func (obj *Falcon) ExportPublicKey(publicKey PublicKey) (*RawPublicKey, error) {
     runtime.KeepAlive(publicKey)
 
     return newRawPublicKeyWithCtx(proxyResult) /* r6 */, nil
-}
-
-/*
-* Return length in bytes required to hold exported public key.
-*/
-func (obj *Falcon) ExportedPublicKeyDataLen(publicKey PublicKey) uint {
-    proxyResult := /*pr4*/C.vscf_falcon_exported_public_key_data_len(obj.cCtx, (*C.vscf_impl_t)(unsafe.Pointer(publicKey.Ctx())))
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(publicKey)
-
-    return uint(proxyResult) /* r9 */
-}
-
-/*
-* Export public key to the raw binary format without algorithm information.
-*
-* Binary format must be defined in the key specification.
-* For instance, RSA public key must be exported in format defined in
-* RFC 3447 Appendix A.1.1.
-*/
-func (obj *Falcon) ExportPublicKeyData(publicKey PublicKey) ([]byte, error) {
-    outBuf, outBufErr := newBuffer(int(obj.ExportedPublicKeyDataLen(publicKey.(PublicKey)) /* lg2 */))
-    if outBufErr != nil {
-        return nil, outBufErr
-    }
-    defer outBuf.delete()
-
-
-    proxyResult := /*pr4*/C.vscf_falcon_export_public_key_data(obj.cCtx, (*C.vscf_impl_t)(unsafe.Pointer(publicKey.Ctx())), outBuf.ctx)
-
-    err := FoundationErrorHandleStatus(proxyResult)
-    if err != nil {
-        return nil, err
-    }
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(publicKey)
-
-    return outBuf.getData() /* r7 */, nil
 }
 
 /*
@@ -348,28 +284,6 @@ func (obj *Falcon) ImportPrivateKey(rawKey *RawPrivateKey) (PrivateKey, error) {
 }
 
 /*
-* Import private key from the raw binary format.
-*/
-func (obj *Falcon) ImportPrivateKeyData(keyData []byte, keyAlgInfo AlgInfo) (PrivateKey, error) {
-    var error C.vscf_error_t
-    C.vscf_error_reset(&error)
-    keyDataData := helperWrapData (keyData)
-
-    proxyResult := /*pr4*/C.vscf_falcon_import_private_key_data(obj.cCtx, keyDataData, (*C.vscf_impl_t)(unsafe.Pointer(keyAlgInfo.Ctx())), &error)
-
-    err := FoundationErrorHandleStatus(error.status)
-    if err != nil {
-        return nil, err
-    }
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(keyAlgInfo)
-
-    return FoundationImplementationWrapPrivateKey(proxyResult) /* r4 */
-}
-
-/*
 * Export private key in the raw binary format.
 *
 * Binary format must be defined in the key specification.
@@ -392,48 +306,6 @@ func (obj *Falcon) ExportPrivateKey(privateKey PrivateKey) (*RawPrivateKey, erro
     runtime.KeepAlive(privateKey)
 
     return newRawPrivateKeyWithCtx(proxyResult) /* r6 */, nil
-}
-
-/*
-* Return length in bytes required to hold exported private key.
-*/
-func (obj *Falcon) ExportedPrivateKeyDataLen(privateKey PrivateKey) uint {
-    proxyResult := /*pr4*/C.vscf_falcon_exported_private_key_data_len(obj.cCtx, (*C.vscf_impl_t)(unsafe.Pointer(privateKey.Ctx())))
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(privateKey)
-
-    return uint(proxyResult) /* r9 */
-}
-
-/*
-* Export private key to the raw binary format without algorithm information.
-*
-* Binary format must be defined in the key specification.
-* For instance, RSA private key must be exported in format defined in
-* RFC 3447 Appendix A.1.2.
-*/
-func (obj *Falcon) ExportPrivateKeyData(privateKey PrivateKey) ([]byte, error) {
-    outBuf, outBufErr := newBuffer(int(obj.ExportedPrivateKeyDataLen(privateKey.(PrivateKey)) /* lg2 */))
-    if outBufErr != nil {
-        return nil, outBufErr
-    }
-    defer outBuf.delete()
-
-
-    proxyResult := /*pr4*/C.vscf_falcon_export_private_key_data(obj.cCtx, (*C.vscf_impl_t)(unsafe.Pointer(privateKey.Ctx())), outBuf.ctx)
-
-    err := FoundationErrorHandleStatus(proxyResult)
-    if err != nil {
-        return nil, err
-    }
-
-    runtime.KeepAlive(obj)
-
-    runtime.KeepAlive(privateKey)
-
-    return outBuf.getData() /* r7 */, nil
 }
 
 /*
--- wrappers/go/foundation/key_alg.go
+++ wrappers/go/foundation/key_alg.go
@@ -48,11 +48,6 @@ type KeyAlg interface {
     ImportPublicKey (rawKey *RawPublicKey) (PublicKey, error)
 
     /*
-    * Import public key from the raw binary format.
-    */
-    ImportPublicKeyData (keyData []byte, keyAlgInfo AlgInfo) (PublicKey, error)
-
-    /*
     * Export public key to the raw binary format.
     *
     * Binary format must be defined in the key specification.
@@ -60,20 +55,6 @@ type KeyAlg interface {
     * RFC 3447 Appendix A.1.1.
     */
     ExportPublicKey (publicKey PublicKey) (*RawPublicKey, error)
-
-    /*
-    * Return length in bytes required to hold exported public key.
-    */
-    ExportedPublicKeyDataLen (publicKey PublicKey) uint
-
-    /*
-    * Export public key to the raw binary format without algorithm information.
-    *
-    * Binary format must be defined in the key specification.
-    * For instance, RSA public key must be exported in format defined in
-    * RFC 3447 Appendix A.1.1.
-    */
-    ExportPublicKeyData (publicKey PublicKey) ([]byte, error)
 
     /*
     * Import private key from the raw binary format.
@@ -88,11 +69,6 @@ type KeyAlg interface {
     ImportPrivateKey (rawKey *RawPrivateKey) (PrivateKey, error)
 
     /*
-    * Import private key from the raw binary format.
-    */
-    ImportPrivateKeyData (keyData []byte, keyAlgInfo AlgInfo) (PrivateKey, error)
-
-    /*
     * Export private key in the raw binary format.
     *
     * Binary format must be defined in the key specification.
@@ -100,20 +76,6 @@ type KeyAlg interface {
     * RFC 3447 Appendix A.1.2.
     */
     ExportPrivateKey (privateKey PrivateKey) (*RawPrivateKey, error)
-
-    /*
-    * Return length in bytes required to hold exported private key.
-    */
-    ExportedPrivateKeyDataLen (privateKey PrivateKey) uint
-
-    /*
-    * Export private key to the raw binary format without algorithm information.
-    *
-    * Binary format must be defined in the key specification.
-    * For instance, RSA private key must be exported in format defined in
-    * RFC 3447 Appendix A.1.2.
-    */
-    ExportPrivateKeyData (privateKey PrivateKey) ([]byte, error)
 
     /*
     * Release underlying C context.
