1cfc111bc232caed8d2b81716c09fbc83e63d43e
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-13 03:17:29 +0300

    Fixing android tests

--- wrappers/java/android/ratchet/src/androidTest/java/com/virgilsecurity/crypto/ratchet/RatchetMessageTest.java
+++ wrappers/java/android/ratchet/src/androidTest/java/com/virgilsecurity/crypto/ratchet/RatchetMessageTest.java
@@ -38,15 +38,13 @@ package com.virgilsecurity.crypto.ratchet;
 
 import android.support.test.runner.AndroidJUnit4;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class RatchetMessageTest {
