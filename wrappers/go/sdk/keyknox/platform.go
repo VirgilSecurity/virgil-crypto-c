7d9078564319f96ec7f6061ffe8a85f12c1b59b0
SergeySeroshtan <sergey.seroshtan@gmail.com>
2021-03-24 13:34:02 +0200

    Replace library nghttp2 with c-ares (build, wrappers, go)

--- wrappers/go/sdk/keyknox/platform.go
+++ wrappers/go/sdk/keyknox/platform.go
@@ -1,11 +1,11 @@
 package sdk_keyknox
 
 // #cgo darwin CFLAGS: -I${SRCDIR}/../../pkg/darwin_amd64/include/
-// #cgo darwin LDFLAGS: -L${SRCDIR}/../../pkg/darwin_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lnghttp2 -lssl -lcrypto -lpthread -ldl
+// #cgo darwin LDFLAGS: -L${SRCDIR}/../../pkg/darwin_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lcares -lssl -lcrypto -lpthread -ldl
 // #cgo linux,!legacy CFLAGS: -I${SRCDIR}/../../pkg/linux_amd64/include/
-// #cgo linux,!legacy LDFLAGS: -L${SRCDIR}/../../pkg/linux_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lnghttp2 -lssl -lcrypto -lpthread -ldl
+// #cgo linux,!legacy LDFLAGS: -L${SRCDIR}/../../pkg/linux_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lcares -lssl -lcrypto -lpthread -ldl
 // #cgo linux,legacy CFLAGS: -I${SRCDIR}/../../pkg/linux_amd64__legacy_os/include/
-// #cgo linux,legacy LDFLAGS: -L${SRCDIR}/../../pkg/linux_amd64__legacy_os/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lnghttp2 -lssl -lcrypto -lpthread -ldl
+// #cgo linux,legacy LDFLAGS: -L${SRCDIR}/../../pkg/linux_amd64__legacy_os/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lcares -lssl -lcrypto -lpthread -ldl
 // #cgo windows CFLAGS: -I${SRCDIR}/../../pkg/windows_amd64/include/
-// #cgo windows LDFLAGS: -L${SRCDIR}/../../pkg/windows_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lnghttp2 -lssl -lcrypto -lpthread -ldl
+// #cgo windows LDFLAGS: -L${SRCDIR}/../../pkg/windows_amd64/lib -lvsc_keyknox_sdk -lvsc_core_sdk -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -ljson-c -lfalcon -lround5 -lcurl -lcares -lssl -lcrypto -lpthread -ldl
 import "C"
