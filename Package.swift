0a70a23f108de4f1dad3c9c92dc13af70f562fc5
SergeySeroshtan <sergey.seroshtan@gmail.com>
2026-02-10 19:27:04 -0500

    Bump version to 0.17.2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let version = "0.17.2-rc15"
+let version = "0.17.2"
 let useLocalBinaries = false
 
 let vscCommonBinaryTarget = {
--- wrappers/python/virgil_crypto_lib/__init__.py
+++ wrappers/python/virgil_crypto_lib/__init__.py
@@ -34,4 +34,4 @@
 
 
 __author__ = "Virgil Security"
-__version__ = "0.17.2-rc15"
+__version__ = "0.17.2"
