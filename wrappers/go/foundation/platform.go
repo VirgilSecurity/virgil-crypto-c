5453b6870400703dd6954e287fed89904b6e09e4
obsidian <sergey.seroshtan@gmail.com>
2026-04-03 09:37:18 -0400

    feat: add Go release workflow with cross-compilation and LFS support (vcc-cwn)
    - Create release-go.yml workflow triggered on v* tags that cross-compiles
      C static libraries for 5 targets (linux/amd64, linux/arm64, darwin/amd64,
      darwin/arm64, windows/amd64), runs Go tests on native targets, commits
      built libraries via Git LFS, and creates Go module tags (wrappers/go/vX.Y.Z)
    - Update CGo platform.go directives in foundation/ and phe/ packages to
      add architecture-specific constraints for darwin/arm64 and linux/arm64
    - Add Git LFS tracking pattern for wrappers/go/pkg/**/*.a in .gitattributes
    - Remove /pkg/ from wrappers/go/.gitignore so built libraries can be committed
    - Fix bug in build-go.yml where macOS arm64 build installed to darwin_amd64
      instead of darwin_arm64

--- wrappers/go/foundation/platform.go
+++ wrappers/go/foundation/platform.go
@@ -1,11 +1,15 @@
 package foundation
 
-// #cgo darwin CFLAGS: -I${SRCDIR}/../pkg/darwin_amd64/include/
-// #cgo darwin LDFLAGS: -L${SRCDIR}/../pkg/darwin_amd64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon
-// #cgo linux,!legacy CFLAGS: -I${SRCDIR}/../pkg/linux_amd64/include/
-// #cgo linux,!legacy LDFLAGS: -L${SRCDIR}/../pkg/linux_amd64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon -lpthread
-// #cgo linux,legacy CFLAGS: -I${SRCDIR}/../pkg/linux_amd64__legacy_os/include/
-// #cgo linux,legacy LDFLAGS: -L${SRCDIR}/../pkg/linux_amd64__legacy_os/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon -lpthread
+// #cgo darwin,amd64 CFLAGS: -I${SRCDIR}/../pkg/darwin_amd64/include/
+// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../pkg/darwin_amd64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon
+// #cgo darwin,arm64 CFLAGS: -I${SRCDIR}/../pkg/darwin_arm64/include/
+// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/../pkg/darwin_arm64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon
+// #cgo linux,amd64,!legacy CFLAGS: -I${SRCDIR}/../pkg/linux_amd64/include/
+// #cgo linux,amd64,!legacy LDFLAGS: -L${SRCDIR}/../pkg/linux_amd64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon -lpthread
+// #cgo linux,amd64,legacy CFLAGS: -I${SRCDIR}/../pkg/linux_amd64__legacy_os/include/
+// #cgo linux,amd64,legacy LDFLAGS: -L${SRCDIR}/../pkg/linux_amd64__legacy_os/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon -lpthread
+// #cgo linux,arm64 CFLAGS: -I${SRCDIR}/../pkg/linux_arm64/include/
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../pkg/linux_arm64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon -lpthread
 // #cgo windows CFLAGS: -I${SRCDIR}/../pkg/windows_amd64/include/
 // #cgo windows LDFLAGS: -L${SRCDIR}/../pkg/windows_amd64/lib -lvsc_foundation -lvsc_foundation_pb -led25519 -lprotobuf-nanopb -lvsc_common -lmbedcrypto -lround5 -lfalcon
 import "C"
